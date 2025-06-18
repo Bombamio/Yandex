9d60919a1264ee510977e093af1cf2c77772b1dd
Yausei Ilya <iliaevsei32@gmail.com>
2025-06-18 13:30:23 +0300

    sql

--- 4. Бэкэнд на Django/Dev/acme_project/main/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-
-def index(request):
-    tamplate_name = 'main/index.html'
-    return render(request, tamplate_name)
-
-    tamplate_name = 'ice_cream/list.html'
-    return render(request, tamplate_name)
